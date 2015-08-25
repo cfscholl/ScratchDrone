95af08bd180abe337383b96ac92bc41c412ac706
Lode Hoste <zillode@zillode.be>
2015-08-25 16:45:02 +0200

    Added scratch to Access-Control-Allow-Origin

--- rest.js
+++ rest.js
@@ -12,6 +12,7 @@ app.use(function (req, res, next) {
 
     // Website you wish to allow to connect
     res.setHeader('Access-Control-Allow-Origin', "http://localhost:"+port);
+    res.setHeader('Access-Control-Allow-Origin', "http://scratchx.org");
 
     // Request methods you wish to allow
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
